b546fd029dcc9a82d89174e3ef756071ae767778
Moreno van Rooijen <m.r.v.rooijen@student.tue.nl>
2018-06-05 15:29:12 +0200

    Setup website

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link type="text/css" rel="stylesheet" href="scss/style.css"></link>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+</head>
+<body>
+	<div class="main-container"id="menu">
+		<?php 
+		include "php/menu.php";
+		?>
+	</div>
+	<div class="main-container" id="home">
+		<div id="title-box"></div>
+		<div id="image-box"></div>
+
+	</div>
+
+	<div class="main-container"id="mains">
+		Main
+	</div>
+
+	<div class="main-container"id="results">
+		Results
+	</div>
+
+
+</body>
+</html>
---
+++ php/menu.php
@@ -0,0 +1,6 @@
+<ul>
+	<li>Test</li>
+	<li>Test2</li>
+	<li>Test3</li>
+
+	</ul>
