7793d226a0f82672a1d4b3d9388062674060ea31
Moreno van Rooijen <m.r.v.rooijen@student.tue.nl>
2018-06-06 13:57:36 +0200

    Remove proc file

--- main.py
+++ main.py
@@ -5,25 +5,36 @@ from os import environ
 app = Flask(__name__)
 api = Api(app)
 names = []
+
+
 class HelloWorld(Resource):
+
     def get(self):
         return{'about':'Hellow Worlds'}
+
     def post(self):
         some_json = request.get_json()
         return {'you sent' : some_json},201
 
+
 class Multi(Resource):
+
     def get(self,num):
         return {'result': num*10}
-class addNames(Resource):
+
+
+class AddNames(Resource):
+
     def get(self,name):
         names.append(name)
         return {
             'you sent': name,
             'all Names': names}
+
+
 api.add_resource(HelloWorld,'/')
 api.add_resource(Multi,'/multi/<int:num>')
-api.add_resource(addNames,'/addName/<string:name>')
+api.add_resource(AddNames,'/addName/<string:name>')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=environ.get("PORT", 5000),debug=False)
