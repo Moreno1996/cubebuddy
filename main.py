1d6f55b88727dd466eb34f14aec62558c0d0e4d8
Moreno van Rooijen <m.r.v.rooijen@student.tue.nl>
2018-06-06 14:27:52 +0200

    Fixed typo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy  import SQLAlchemy
 from flask_restful import Resource, Api
 from os import environ
 
